01399b85589144ff1671beda538fc13cd4923b60
Mr-4000 <wangzch99@gmail.com>
2019-09-22 19:43:06 -0400

    xx

---
+++ 46. Permutations/Permutations_1.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(first = 0):
+            # if all integers are used up
+            if first == n:  
+                output.append(nums[:])
+            for i in range(first, n):
+                # place i-th integer first 
+                # in the current permutation
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
+#Runtime: 20 ms, faster than 96.46% of Python online submissions for Permutations.
+#Memory Usage: 11.9 MB, less than 56.00% of Python online submissions for Permutations.
