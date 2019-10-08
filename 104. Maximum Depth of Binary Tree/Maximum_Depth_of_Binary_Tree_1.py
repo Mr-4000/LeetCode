337b288d0aa588c280e52cca2fbf01338a864e38
Mr-4000 <wangzch99@gmail.com>
2019-10-08 19:48:43 -0400

    Create Maximum_Depth_of_Binary_Tree_1.py

---
+++ 104. Maximum Depth of Binary Tree/Maximum_Depth_of_Binary_Tree_1.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """ 
+        if root is None: 
+            return 0 
+        else: 
+            left_height = self.maxDepth(root.left) 
+            right_height = self.maxDepth(root.right) 
+            return max(left_height, right_height) + 1 
+#Runtime: 24 ms, faster than 93.85% of Python online submissions for Maximum Depth of Binary Tree.
+#Memory Usage: 14.6 MB, less than 77.78% of Python online submissions for Maximum Depth of Binary Tree.
